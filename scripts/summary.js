94156eaf4558dc58fd103338ae86b76462368600
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-03-04 13:20:01 +0100

    greeting func implemented w. name

--- scripts/summary.js
+++ scripts/summary.js
@@ -2,5 +2,36 @@
 
 const initSummary = async () => {
 	await loadSideMenuHeader();
+	renderGreeting();
 	document.getElementById('summary-btn').classList.add('active');
+};
+
+/**
+ * Gets time in hours
+ * @returns {number} hours
+ */
+const timeOfDay = () => {
+	const date = new Date();
+	const hours = date.getHours();
+	return hours;
+};
+
+/**
+ * Depending on time returns greeting
+ * @param {number} hours
+ * @returns string fitting greeting
+ */
+const getGreeting = () => {
+	const hours = timeOfDay();
+	if (hours >= 0 && hours < 12) return 'Good Morning,';
+	if (hours >= 12 && hours < 18) return 'Good Day,';
+	return 'Good Evening,';
+};
+
+/**
+ * Renders greeting
+ */
+const renderGreeting = () => {
+	getGreeting();
+	document.getElementById('greeting-title').innerHTML = getGreeting();
 };
